fed34a32ed34d289b76257c8f2355ac3280121b6
Andrew O'Hara <andrew@andrewohara.io>
2017-11-04 14:51:40 -0400

    make boto3 an optional dependency #1

--- setup.py
+++ setup.py
@@ -9,12 +9,9 @@ with open(README_FILENAME, 'r') as f:
 
 setup(
     name='serverless-config',
-    version='0.2.1',
+    version='0.2.2.dev2',
 
     packages=['serverless_config'],
-
-    # dependencies:
-    install_requires=['boto3'],
 
     # PyPI metadata
     author='Andrew O\'Hara',
